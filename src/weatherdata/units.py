96a0042717e4320b438fbacbcd9e68f66b43d6ed
Marc LABADIE <labadiemarc89@gmail.com>
2022-01-07 08:24:10 +0100

    test units definition and conversion with pyunits

---
+++ src/weatherdata/units.py
@@ -0,0 +1,20 @@
+"""
+Units module of weather data derived from pint library to manage units
+"""
+
+import numpy as np
+import pint
+import pint.unit
+import weatherdata
+
+
+# define registry see pint
+units = pint.UnitRegistry()
+Quantity= units.Quantity
+
+units.define('wattPar = 1*watt*meter * meter = W_m²')
+units.define('PPFD = wattPar / 4.6 / 0.48 = ppfd')
+units.define('@alias celsius = Celcius')
+
+
+del pint
